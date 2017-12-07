32c365e536988a29cc0add22af389c3026effccf
Maksim Kholod <maksi123p@gmail.com>
2017-12-07 21:10:15 +0200

    No display

--- list-price2/options.php
+++ list-price2/options.php
@@ -1,5 +1,25 @@
 <?php if (!defined('FW')) die('Forbidden');
+
 $options = array(
+    'title-service' => array(
+        'type' => 'text',
+        'value' => 'default value',
+        'attr' => array('class' => 'custom-class', 'data-foo' => 'bar'),
+        'label' => __('title-service', ''),
+        'desc' => __('Description', ''),
+        'help' => __('Help tip', ''),
+    ),
+
+    'img-service' => array(
+        'label' => __('img-service', ''),
+        'type' => 'upload',
+        'images_only' => true,
+        'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
+        'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
+            __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
+            __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
+        ),
+    ),
 
     'addable' => array(
         'type' => 'addable-popup',
@@ -10,7 +30,7 @@ $options = array(
             ),
         ),
         'label' => __('Addable Popup', ''),
-        'desc'  => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
+        'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
         'template' => '{{- item-prise }}',
         'popup-title' => null,
         'size' => 'small', // small, medium, large
@@ -18,107 +38,26 @@ $options = array(
         'add-button-text' => __('Add', ''),
         'sortable' => true,
         'popup-options' => array(
-            'option_1' => array(
-                'label' => __('Text', ''),
+            'service' => array(
+                'label' => __('service', ''),
                 'type' => 'text',
-                'value' => 'text value',
+                'value' => 'service',
                 'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
                 'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
                     __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
                     __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '{domain}')
                 ),
             ),
-            'option_2' => array(
-                'label' => __('Text', ''),
+            'price' => array(
+                'label' => __('price', ''),
                 'type' => 'text',
-                'value' => 'textarea value',
+                'value' => 'price',
                 'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
                 'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
                     __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_3' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_4' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_5' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_6' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_7' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_8' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_9' => array(
-                'label' => __('Text', ''),
-                'type' => 'text',
-                'value' => 'textarea value',
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
-                ),
-            ),
-            'option_10' => array(
-                'label' => __('img', ''),
-                'type'  => 'upload',
-                'images_only' => true,
-                'desc' => __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                'help' => sprintf("%s \n\n'\"<br/><br/>\n\n <b>%s</b>",
-                    __('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', ''),
-                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '')
+                    __('Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium', '{domain}')
                 ),
             ),
         ),
     )
-
 );
--- list-price2/views/view.php
+++ list-price2/views/view.php
@@ -1,51 +1,35 @@
 <?php if (!defined('FW')) {
     die('Forbidden');
 }
+$a = 1;
 ?>
+
 <div>
+
+        <?php if ($atts['title-service']) { ?>
+            <h2 class="title-price"><?php echo $atts['title-service']; ?></h2>
+        <?php } ?>
+
+        <?php if ($atts['img-service']['url']) { ?>
+            <h3 class="title-price"><?php echo $atts['img-service']['url']; ?></h3>
+        <?php } ?>
+
+
     <?php if ($atts['addable']) { ?>
         <?php foreach ($atts['addable'] as $item) { ?>
             <div class="wrap-title-service">
                 <h2 class="title">
-                    <i class="fa fa-caret-right" aria-hidden="true"></i><?php echo $item['option_1']; ?></h2>
-                <div>
-                    <?php if ($item['option_10']['url']) { ?>
-                        <img src="<?php echo $item['option_10']['url']; ?>">
-                    <?php } ?>
-                </div>
+                    <i class="fa fa-caret-right" aria-hidden="true"></i><?php echo $item['title-service']; ?></h2>
+
 
             </div>
             <ul class="wrap-title-price">
                 <li>
-                    <?php if ($item['option_2']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_2']; ?></h3>
+                    <?php if ($item['service']) { ?>
+                        <h3 class="title-price"><?php echo $item['service']; ?></h3>
                     <?php } ?>
-                    <?php if ($item['option_3']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_3']; ?></h3>
-                    <?php } ?>
-                </li>
-                <li>
-                    <?php if ($item['option_4']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_4']; ?></h3>
-                    <?php } ?>
-                    <?php if ($item['option_5']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_5']; ?></h3>
-                    <?php } ?>
-                </li>
-                <li>
-                    <?php if ($item['option_6']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_6']; ?></h3>
-                    <?php } ?>
-                    <?php if ($item['option_7']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_7']; ?></h3>
-                    <?php } ?>
-                </li>
-                <li>
-                    <?php if ($item['option_8']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_8']; ?></h3>
-                    <?php } ?>
-                    <?php if ($item['option_9']) { ?>
-                        <h3 class="title-price"><?php echo $item['option_9']; ?></h3>
+                    <?php if ($item['price']) { ?>
+                        <h3 class="title-price"><?php echo $item['price']; ?></h3>
                     <?php } ?>
                 </li>
             </ul>
